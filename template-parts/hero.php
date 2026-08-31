8553947a99729450561acb6416ea234dc6beb8b9
Damien <damien.h68@gmail.com>
2026-08-31 21:01:47 +0200

    Fix hero subtitle args

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -17,7 +17,7 @@
 $hero_image_id = $args['image_id'] ?? travel_dams_get_hero_image_id();
 $hero_title    = $args['title'] ?? travel_dams_get_hero_title();
 // $hero_subtitle = $args['subtitle'] ?? travel_dams_get_hero_title();
-$hero_subtitle = $args['subtitle'];
+$hero_subtitle = $args['subtitle'] ?? '';
 $hero_eyebrow  = $args['eyebrow'] ?? '';
 $hero_byline   = $args['byline'] ?? '';
 $hero_ctas     = $args['ctas'] ?? array();
